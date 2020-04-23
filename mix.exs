4a7273b1f7dbe98350f15c08a130126491681ab3
David Astor <toodimes@gmail.com>
2020-04-23 10:12:50 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FirebaseAdminEx.MixProject do
     [
       app: :firebase_admin_ex,
       version: "0.2.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
